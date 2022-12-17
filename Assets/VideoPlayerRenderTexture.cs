b1c75a8cccede17842fb1baffe419f44dcae1162
nattos <natnyo@gmail.com>
2022-12-17 11:45:27 +0000

    Cleanup of unused resources on branch.

--- Assets/VideoPlayerRenderTexture.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.Video;
-
-public class VideoPlayerRenderTexture : MonoBehaviour {
-  public void Start() {
-    GetComponent<VideoPlayer>().targetTexture = new RenderTexture(1920, 1080, 0, RenderTextureFormat.ARGB32);
-  }
-}
